5b283e21f79375024ca1c438ef03b56b0dcf5bbf
keyshaasnoopy <keysharianty403@gmail.com>
2025-05-27 10:00:26 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.image("a5aa66feb1115140297b638bc1876240.jpg", width=200)
 
 st.title("aplikasi sederhana") 
 st.header("aplikasi mengecek nilai genap/ganjil")
-angka = st.number_input(tulis sebuah angka:" , value=0, step=1)
+angka = st.number_input("tulis sebuah angka:" , value=0, step=1)
 
 if (angka % 2) == 0:
     st write(f"{angka} adalah bilangan genap")
